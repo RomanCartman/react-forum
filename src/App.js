f989ff08e938abe05808d122725e4d2a4bafd0d5
RomanCartman <romanas911@icloud.com>
2025-04-10 13:39:55 +0800

    Тестовые защищеные страницы

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import Home from './pages/Home/Home';
 import Courses from './pages/Courses/Courses';
 import Login from './pages/Login/Login';
 import Register from './pages/Register/Register';
+import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 function App() {
   return (
@@ -18,7 +19,11 @@ function App() {
           <main className="content">
             <Routes>
               <Route path="/" element={<Home />} />
-              <Route path="/courses" element={<Courses />} />
+              <Route path="/courses" element={
+                  <ProtectedRoute>
+                    <Courses />
+                  </ProtectedRoute>
+                } />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
             </Routes>
